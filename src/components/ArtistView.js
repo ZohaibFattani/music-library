3081261a395d4fc589564cf8bdc689a1cd7efe32
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-25 19:04:02 -0700

    added home and back buttons

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function ArtistView() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [artistData, setArtistData] = useState([]);
-  const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
-
 
   useEffect(() => {
     const API_URL = "http://localhost:4000/album/${id}";
@@ -16,6 +16,36 @@ function ArtistView() {
     };
     fetchData();
   }, [id]);
+
+  const justAlbums = artistData.filter(
+    (entry) => entry.collectionType === "Album"
+  );
+  const renderAlbums = justAlbums.map((album, i) => {
+    return (
+      <div key="">
+        <Link to={`/album/${album.collectionId}`}>
+          <p>{album.collectionName}</p>
+        </Link>
+      </div>
+    );
+  });
+
+  const navButtons = () => {
+    return (
+      <div>
+        <button onClick={() => navigate(-1)}>Back</button>|
+        <button onClick={() => navigate("/")}>Home</button>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+        {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
+        {navButtons()}
+        {renderAlbums}
+    </div>
+)
 }
 
 export default ArtistView;
